7ff8c1e945bdf179ccd286a8ea105586318cc06e
KerrWu <wz_0818@163.com>
2019-07-18 19:01:47 +0800

    add FPN to network.py and resnet_v1.py

--- lib/nets/resnet_v1.py
+++ lib/nets/resnet_v1.py
@@ -241,6 +241,9 @@ class resnetv1(Network):
             if "fpn" in v.name:
                 continue
 
+            if "block4" in v.name:
+                continue
+
             if v.name.split(':')[0] in var_keep_dic:
                 print('Variables restored: %s' % v.name)
                 print(v.get_shape().as_list(), var_keep_dic[v.name.split(':')[0]])
