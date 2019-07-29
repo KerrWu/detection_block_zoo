1424f38d2be34223914c1a3ac0db7c80c73d073d
KerrWu <wz_0818@163.com>
2019-07-29 19:30:53 +0800

    .

--- lib/nets/resnet_v1.py
+++ lib/nets/resnet_v1.py
@@ -208,7 +208,6 @@ class resnetv1(Network):
             with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
 
                 pool5 = resnet_utils.conv2d_same(pool5, 512, 1, stride=1)
-
                 fc7, _ = resnet_v1.resnet_v1(pool5,
                                              self._blocks[-1:],
                                              global_pool=False,
